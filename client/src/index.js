0a113dc3d88f6b2238198fb4ac27b71dd851b495
Renat Khakimov <alldeveloper@yandex.ru>
2021-05-26 13:32:15 +0300

    Addid moment in dependency

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './copmonents/App';
 import reportWebVitals from './reportWebVitals';
 
-// import WebSock from './WebSock';
+// import Emoji from './copmonents/Emoji/emoji';
 
 ReactDOM.render(
   <React.StrictMode>
